510799c36a0db258bd53c15a3c747b4c578ab9de
Jikku Jose <jikkujose@gmail.com>
2015-01-12 12:02:15 +0530

    Add solution for 'leap-year'

--- ruby/leap/leap_test.rb
+++ ruby/leap/leap_test.rb
@@ -1,6 +1,8 @@
 require 'date'
 require 'minitest/autorun'
 require_relative 'year'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new({color: true})]
 
 class Date
   def leap?
@@ -11,28 +13,24 @@ class Date
   alias_method :julian_leap?, :leap?
 end
 
-class YearTest < MiniTest::Unit::TestCase
+class YearTest < Minitest::Test
   def test_leap_year
     assert Year.leap?(1996), 'Yes, 1996 is a leap year'
   end
 
   def test_non_leap_year
-    skip
     refute Year.leap?(1997), 'No, 1997 is not a leap year'
   end
 
   def test_non_leap_even_year
-    skip
     refute Year.leap?(1998), 'No, 1998 is not a leap year'
   end
 
   def test_century
-    skip
     refute Year.leap?(1900), 'No, 1900 is not a leap year'
   end
 
   def test_fourth_century
-    skip
     assert Year.leap?(2400), 'Yes, 2400 is a leap year'
   end
 end
