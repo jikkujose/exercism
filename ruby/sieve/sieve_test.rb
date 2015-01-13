e8e60868a4ee4a9f1a7130112935967c0ef6bd31
Jikku Jose <jikkujose@gmail.com>
2015-01-13 11:09:27 +0530

    Add solution for 'sieve'

--- ruby/sieve/sieve_test.rb
+++ ruby/sieve/sieve_test.rb
@@ -1,14 +1,15 @@
 require 'minitest/autorun'
 require_relative 'sieve'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new({color: true})]
 
-class SieveTest < MiniTest::Unit::TestCase
+class SieveTest < Minitest::Test
   def test_a_few_primes
     expected = [2, 3, 5, 7]
     assert_equal expected, Sieve.new(10).primes
   end
 
   def test_primes # rubocop:disable Metrics/MethodLength
-    skip
     expected = [
       2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
       61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127,
