37e87abead63f24636baaeeaeb1897f859e6119b
Jikku Jose <jikkujose@gmail.com>
2015-01-22 13:00:07 +0530

    Add solution for 'beer-song'

--- ruby/beer-song/beer_song_test.rb
+++ ruby/beer-song/beer_song_test.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
 require_relative 'beer_song'
+require 'minitest/reporters'
+Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new({color: true})]
 
-class BeerSongTest < MiniTest::Unit::TestCase
+class BeerSongTest < Minitest::Test
   def song
     @song = ::BeerSong.new
   end
@@ -17,28 +19,24 @@ class BeerSongTest < MiniTest::Unit::TestCase
   end
 
   def test_another_typical_verse
-    skip
     expected = "3 bottles of beer on the wall, 3 bottles of beer.\n" \
       "Take one down and pass it around, 2 bottles of beer on the wall.\n"
     assert_equal expected, song.verse(3)
   end
 
   def test_verse_1
-    skip
     expected = "1 bottle of beer on the wall, 1 bottle of beer.\n" \
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
     assert_equal expected, song.verse(1)
   end
 
   def test_verse_2
-    skip
     expected = "2 bottles of beer on the wall, 2 bottles of beer.\n" \
       "Take one down and pass it around, 1 bottle of beer on the wall.\n"
     assert_equal expected, song.verse(2)
   end
 
   def test_verse_0
-    skip
     expected =
       "No more bottles of beer on the wall, no more bottles of beer.\n" \
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
@@ -46,7 +44,6 @@ class BeerSongTest < MiniTest::Unit::TestCase
   end
 
   def test_several_verses
-    skip
     expected = "8 bottles of beer on the wall, 8 bottles of beer.\n" \
       "Take one down and pass it around, 7 bottles of beer on the wall.\n\n" \
       "7 bottles of beer on the wall, 7 bottles of beer.\n" \
@@ -57,7 +54,6 @@ class BeerSongTest < MiniTest::Unit::TestCase
   end
 
   def test_the_whole_song
-    skip
     assert_equal song.verses(99, 0), song.sing
   end
 end
