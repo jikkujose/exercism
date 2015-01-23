e91b69480dac2a9720be29bcd5b495d5208cb6c7
Jikku Jose <jikkujose@gmail.com>
2015-01-23 07:27:42 +0530

    Add solution for 'space-age'

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,13 +1,30 @@
 class SpaceAge
+  attr_reader :seconds
+
+  ORBITAL_PERIOD = {
+    earth: 1,
+    mercury:  0.2408467,
+    venus:  0.61519726,
+    mars:  1.8808158,
+    jupiter:  11.862615,
+    saturn:  29.447498,
+    uranus:  84.016846,
+    neptune:  164.79132
+  }
+
   def initialize(seconds)
     @seconds = seconds
   end
 
-  def seconds
-    @seconds
+  ORBITAL_PERIOD.each do |planet, period|
+    define_method "on_#{planet}" do
+      (earth_year / period).round(2)
+    end
   end
 
-  def on_earth
-    (@seconds / (60 * 60 * 24 * 365.25)).round(2)
+  private
+
+  def earth_year
+    @seconds / (60 * 60 * 24 * 365.25)
   end
 end
