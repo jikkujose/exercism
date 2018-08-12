860c35f9d6b98678cf09ca3503f5fee8724dd465
Jikku Jose <jikkujose@gmail.com>
2018-08-12 23:43:06 +0530

    es6 | solution for roman-numerals

--- ecmascript/roman-numerals/roman-numerals.js
+++ ecmascript/roman-numerals/roman-numerals.js
@@ -1,9 +1,20 @@
+export default function toRoman(number) {
+  const unitPlace = number % 10
+  const tensPlace = Math.floor(number / 10) % 10
+  const hundredsPlace = Math.floor(number / 100) % 10
+  const thousandsPlace = Math.floor(number / 1000) % 10
+
+  return `${thousands(thousandsPlace)}${hundreds(hundredsPlace)}${tens(
+    tensPlace
+  )}${units(unitPlace)}`
+}
+
 const repeat = (n, c) =>
   Array(n)
     .fill(c)
     .join("")
 
-const tens = number => {
+const units = number => {
   switch (number) {
     case 1:
     case 2:
@@ -25,20 +36,53 @@ const tens = number => {
   }
 }
 
-const hundreds = number => {
+const tens = number => {
   switch (number) {
     case 1:
     case 2:
     case 3:
-      return repeat(number - 1, "X")
+      return repeat(number, "X")
+    case 4:
+      return "XL"
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      return `L${repeat(number - 5, "X")}`
+    case 9:
+      return "XC"
     default:
       return ""
   }
 }
 
-export default function toRoman(number) {
-  const unitPlace = number % 10
-  const tensPlace = Math.round(number / 10) % 10
+const hundreds = number => {
+  switch (number) {
+    case 1:
+    case 2:
+    case 3:
+      return repeat(number, "C")
+    case 4:
+      return "CD"
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+      return `D${repeat(number - 5, "C")}`
+    case 9:
+      return "CM"
+    default:
+      return ""
+  }
+}
 
-  return `${hundreds(tensPlace)}${tens(unitPlace)}`
+const thousands = number => {
+  switch (number) {
+    case 1:
+    case 2:
+    case 3:
+      return repeat(number, "M")
+    default:
+      return ""
+  }
 }
